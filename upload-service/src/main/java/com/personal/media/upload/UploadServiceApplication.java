e780227a806a0b1349c0e8a76b136eec21fd22ca
Anh Tran <anhtm621@gmail.com>
2026-04-06 20:56:30 +0700

    feature: Added storage service and storage location property

--- upload-service/src/main/java/com/personal/media/upload/UploadServiceApplication.java
+++ upload-service/src/main/java/com/personal/media/upload/UploadServiceApplication.java
@@ -1,9 +1,12 @@
 package com.personal.media.upload;
 
+import com.personal.media.upload.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadServiceApplication {
 
     public static void main(String[] args) {
---
+++ upload-service/src/main/java/com/personal/media/upload/service/FileSystemStorageService.java
@@ -0,0 +1,75 @@
+package com.personal.media.upload.service;
+
+import com.personal.media.upload.config.StorageProperties;
+import com.personal.media.upload.exception.StorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
+public class FileSystemStorageService {
+
+    private final Path rootLocation;
+
+    @Autowired
+    public FileSystemStorageService(StorageProperties properties) {
+        if (properties.getLocation().trim().isEmpty()) {
+            throw new StorageException("File upload location can not be Empty.");
+        }
+
+        this.rootLocation = Paths.get(properties.getLocation());
+        init();
+    }
+
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+
+    public String store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+            String originalFilename = file.getOriginalFilename();
+            String extension = "";
+            if (originalFilename != null && originalFilename.contains(".")) {
+                extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            }
+            
+            // Generate a unique filename to avoid overriding
+            String newFilename = UUID.randomUUID() + extension;
+            Path destinationFile = this.rootLocation.resolve(
+                    Paths.get(newFilename))
+                    .normalize().toAbsolutePath();
+                    
+            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check to prevent directory traversal attacks
+                throw new StorageException(
+                        "Cannot store file outside current directory.");
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+                return newFilename;
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
+
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+}
