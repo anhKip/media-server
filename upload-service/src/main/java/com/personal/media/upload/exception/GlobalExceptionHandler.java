08eb5a192bb79655c7ab239454809a2c97e1c0dc
anhKip <anhtm621@gmail.com>
2026-04-08 19:17:12 +0700

    refractor: removed duplicated import and renamed handle exception method

--- upload-service/src/main/java/com/personal/media/upload/exception/GlobalExceptionHandler.java
+++ upload-service/src/main/java/com/personal/media/upload/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.personal.media.upload.exception;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
@@ -13,7 +12,7 @@ import com.personal.media.common.dto.UploadResponseDTO;
 public class GlobalExceptionHandler {
     
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<UploadResponseDTO> handleStorageException(MaxUploadSizeExceededException ex) {
+    public ResponseEntity<UploadResponseDTO> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
         UploadResponseDTO response = new UploadResponseDTO("", "", "File size exceeds the maximum allowed size (10MB).", "FAILED", 0);
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(response);
     }
