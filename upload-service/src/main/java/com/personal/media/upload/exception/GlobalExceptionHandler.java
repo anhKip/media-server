ab6669f786ac717c44239e2053d58c1e5567abb1
Anh Tran <anhtm621@gmail.com>
2026-04-08 11:40:42 +0700

    feature: global exception handler for max upload size exceeded exception

---
+++ upload-service/src/main/java/com/personal/media/upload/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.personal.media.upload.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import com.personal.media.common.dto.UploadResponseDTO;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<UploadResponseDTO> handleStorageException(MaxUploadSizeExceededException ex) {
+        UploadResponseDTO response = new UploadResponseDTO("", "", "File size exceeds the maximum allowed size (10MB).", "FAILED", 0);
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(response);
+    }
+}
